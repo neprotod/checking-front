694c5789304504d1ad99023daa29aeac5a6cd229
Anastasija3587 <anastasija3587@gmail.com>
2020-02-29 14:51:21 +0200

    added api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,28 @@
 import axios from 'axios';
 import config from '../config';
 
-export default {
-  // TODO: All methods, all privet method must have prefix _
-}
+axios.defaults.baseURL = config.baseUrl;
+
+export const register = user => axios.post('/user/register', user);
+
+export const login = user => axios.post('user/login', user);
+
+export const logout = () => axios.get('/logout');
+
+export const getTask = id => axios.get(`/tasks/${id}`);
+
+export const getTasks = value => axios.get(`/tasks?filter=${value}`);
+
+export const createTask = task => axios.post('/tasks', task);
+
+export const updateTask = (id, value) => axios.put(`/tasks${id}`, value);
+
+export const deleteTask = id => axios.delete(`/tasks/${id}`);
+
+export const getAllPriority = () => axios.get('/tasks/priority');
+
+export const getAllUserRoles = () => axios.get('/user/roles');
+
+export const createRole = role => axios.post('/user/roles', role);
+
+export const deleteRole = id => axios.delete(`/user/roles/${id}`);
