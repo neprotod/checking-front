fb3043ed686651054c7af52db5502de87187387f
Ivan <lysko1999i@gmail.com>
2020-03-01 02:00:50 +0200

    return info inde.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ErrorBoundary from './components/ErrorBoundary';
 import App from './components/App';
 
 // TODO: import ApiProvider and self api, ApiProvider have value and this value needs api
+// const moviedb = new Moviedb();
 
 ReactDOM.render(
   // TODO: Wrap in apiProvider and ErrorBoundary
