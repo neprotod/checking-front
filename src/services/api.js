69e80d28afb90ab85d9dc8d6f9c558bb7a1cd158
Anastasija3587 <anastasija3587@gmail.com>
2020-03-01 19:17:39 +0200

    fixed api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,26 +3,41 @@ import config from '../config';
 
 axios.defaults.baseURL = config.baseUrl;
 
-export const register = user => axios.post('/user/register', user);
-
-export const login = user => axios.post('user/login', user);
-
-export const logout = () => axios.get('/logout');
-
-export const getTask = id => axios.get(`/tasks/${id}`);
-
-export const getTasks = value => axios.get(`/tasks?filter=${value}`);
-
-export const createTask = task => axios.post('/tasks', task);
-
-export const updateTask = (id, value) => axios.put(`/tasks${id}`, value);
-
-export const deleteTask = id => axios.delete(`/tasks/${id}`);
-
-export const getAllPriority = () => axios.get('/tasks/priority');
-
-export const getAllUserRoles = () => axios.get('/user/roles');
-
-export const createRole = role => axios.post('/user/roles', role);
-
-export const deleteRole = id => axios.delete(`/user/roles/${id}`);
+export default {
+  register(user) {
+    axios.post('user/register', user);
+  },
+  login(user) {
+    axios.post('user/login', user);
+  },
+  logout() {
+    axios.get('user/logout');
+  },
+  getTask(id) {
+    axios.get(`tasks/${id}`);
+  },
+  getTasks(value = '') {
+    axios.get(`tasks?filter=${value}`);
+  },
+  createTask(task) {
+    axios.post('tasks', task);
+  },
+  updateTask(id, value) {
+    axios.put(`tasks${id}`, value);
+  },
+  deleteTask(id) {
+    axios.delete(`tasks/${id}`);
+  },
+  getAllPriority() {
+    axios.get('tasks/priority');
+  },
+  getAllUserRoles() {
+    axios.get('user/roles');
+  },
+  createRole(role) {
+    axios.post('user/roles', role);
+  },
+  deleteRole(id) {
+    axios.delete(`user/roles/${id}`);
+  },
+};
