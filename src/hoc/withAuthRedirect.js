e17520c435df4403f9c9c7c100ff42153e6eb316
Anastasija3587 <anastasija3587@gmail.com>
2020-03-08 18:01:29 +0200

    done google auth and added css

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -6,12 +6,7 @@ import * as authSelectors from '../redux/auth/authSelectors';
 
 const withAuthRedirect = BaseComponent => {
   const WithAuthRedirect = ({ isAuth, ...restProps }) => {
-    return isAuth ? (
-      <Redirect to="/main" />
-    ) : (
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      <BaseComponent {...restProps} />
-    );
+    return isAuth ? <Redirect to="/main" /> : <BaseComponent {...restProps} />;
   };
   WithAuthRedirect.propTypes = {
     isAuth: PropTypes.bool.isRequired,
