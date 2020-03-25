583a9d531f9b3c16547c8b1f6363b87196e28ab0
Irina <irina.lysenko21@gmail.com>
2020-03-25 11:24:02 +0200

    loader css fixed

--- src/components/CreateTask/Loader/Loader.js
+++ src/components/CreateTask/Loader/Loader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import LoaderComponent from 'react-loader-spinner';
 
 const Loader = () => (
-  <LoaderComponent type="ThreeDots" color="#ff6b08" height={20} width={100} />
+  <LoaderComponent type="ThreeDots" color="#ff6b08" height={18} width={100} />
 );
 
 export default Loader;
